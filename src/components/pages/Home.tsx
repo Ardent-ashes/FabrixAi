34f859ecc12ad8772fbfae564e1a6217d80b1458
Ardent-ashes <papry-2021811177@cs.du.ac.bd>
2025-05-29 23:08:40 +0600

    Homepage fixed

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,56 +1,33 @@
 // components/pages/About.tsx
-import logoImage from "../../assets/logo.png"; 
-import Name from "../../assets/name.png"; 
+import logoImage from "../../assets/logo.png";
+import Name from "../../assets/name.png";
 import { useNavigate } from 'react-router-dom';
-
-
-
-
-
 
 function Home() {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate('/signup');
   };
+
   return (
-    <div className="gradient-background flex justify-center items-center min-h-screen p-5"  onClick={handleClick}>
-      <div className="w-full max-w-6xl flex flex-col items-center z-20">
-        <img 
-          src={logoImage}  
-          alt="About Us"
-          style={{
-           
-            position: 'absolute',
-            left: '50%',
-            top: '40%',
-            transform: 'translate(-50%, -50%)',
-            maxWidth: '300px',
-            height: 'auto'
-
-          }} 
-        />
-      
+    <div className="gradient-background flex justify-center items-center min-h-screen p-5" onClick={handleClick}>
+      <div className="w-full max-w-6xl flex flex-col items-center justify-center z-20 space-y-8">
+        <div className="flex justify-center">
+          <img 
+            src={logoImage} 
+            alt="Logo"
+            className="max-w-[300px] h-auto"
+          />
+        </div>
         
+        <div className="flex justify-center">
+          <img 
+            src={Name}
+            alt="Name"
+            className="max-w-[300px] h-auto"
+          />
+        </div>
       </div>
-      
-      <div className="flex justify-center pb-6">
-        <img 
-          src={Name}  // Use the imported variable
-          alt="About Us"
-          style={{
-           
-            position: 'absolute',
-            left: '50%',
-            top: '60%',
-            transform: 'translate(-50%, -50%)',
-            maxWidth: '300px',
-            height: 'auto'
-
-          }} 
-        />
-      </div>
-
     </div>
   );
 }
